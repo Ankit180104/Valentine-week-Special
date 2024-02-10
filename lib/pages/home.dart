b5e6a2617ec34a67aab34747f1061d73ec0da1a6
Ankit Kumar Sharma <sharmaankitkumar33@gmail.com>
2024-02-10 19:36:03 +0530

    corrected contents

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -170,6 +170,9 @@ class HomeScreen extends StatelessWidget {
       "day": "7 Rose Day",
       "event": "Express your love with roses",
       "additionalInfo": {
+        "How to select Roses":{
+          "site you can visit":"https://indianexpress.com/article/what-is/rose-day-2024-from-date-to-significance-everything-you-need-to-know-9146639/"
+        },
         "Roses you can Buy": {
           "flower bouquet":
               "https://www.amazon.in/s?k=flower+bouquet&crid=9VWYNLX3SBXH&sprefix=flowe+bouque%2Caps%2C309&ref=nb_sb_noss_2",
@@ -200,7 +203,6 @@ class HomeScreen extends StatelessWidget {
           "Guftagu Cafe":
               "https://www.google.com/maps/dir/NSUT,+Azad+Hind+Fauj+Marg,+Dwarka+Sector-3,+Dwarka,+Delhi/Guftagu+Cafe,+34+Main+Road,+DLF+Phase+2,+Sector+25,+Gurugram,+Haryana/@28.5468379,76.9809903,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390d05dd375e5a13:0x108adaa3abe4bd07!2m2!1d77.0379647!2d28.6100216!1m5!1m1!1s0x390d192e241cf5ff:0xe07747ba5a08dbf0!2m2!1d77.094825!2d28.4852699?entry=ttu",
           "Big tree Cafe": "https://www.google.com/maps/dir/NSUT,+Azad+Hind+Fauj+Marg,+Dwarka+Sector-3,+Dwarka,+Delhi/The+Big+Tree+Cafe,+opposite+Paras+Quartier,+Gwal+Pahari,+Gurugram,+Haryana/@28.5223577,76.9992864,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390d05dd375e5a13:0x108adaa3abe4bd07!2m2!1d77.0379647!2d28.6100216!1m5!1m1!1s0x390d19f50603f797:0x58d281926f5de632!2m2!1d77.1323344!2d28.4364957?entry=ttu",
-          
         },
         "Proposing Quotes": {
           "Propose using these beautiful quotes": "https://awbi.in/happy-propose-day-2024-wishes-quotes-messages/"
@@ -258,40 +260,41 @@ class HomeScreen extends StatelessWidget {
       "day": "12 Hug Day",
       "event": "Show your love with a warm hug",
       "additionalInfo": {
-        "Hug day": {
-          "perfect places": [
+        "Perfect places to visit": {
+          "Perfect places for the day": [
             "THE GARDEN OF FIVE SENSES ",
             "JHEEL PARK",
             "ROSE CAFE ",
             "PACIFIC MALL",
-            "VEGAS MALL"
           ],
-          "gifts": {
-            "amazon": "https://www.amazon.in/Hug-Day-Gift/s?k=Hug+Day+Gift",
+          },
+        "Things to make the day special": {
+            "Gifts": "https://www.amazon.in/Hug-Day-Gift/s?k=Hug+Day+Gift",
             "FNP":
                 "https://www.fnp.com/gifts/hug-day-lp?utm_source=Google&utm_medium=cpc&utm_campaign=165874490&utm_adgroup=135962746761&utm_keyword=hug%20day%20gifts&utm_device=c&utm_placement=&utm_network=g&gad_source=1&gclid=CjwKCAiAq4KuBhA6EiwArMAw1IViRSo2Fi32bsMM-ddZLfgumbnj8rD6iP5AHmXY8wzMVU9oYjjoBhoC9IoQAvD_BwE",
-            "Flower aura":
+            "Memories":
                 "https://www.floweraura.com/hug-day?gclid=CjwKCAiAq4KuBhA6EiwArMAw1OsxwBkYXDKLQGV8Cz_BfUgvxvpCQVxyIIfz6BS3W52LfKmnGy6CHBoCcp4QAvD_BwE:G:s&keyword=happy+hug+day+gift&fa-matchtype=p&fa-adid=645965398254&fa-network=g&fa-device=c&fa-devicemodel=&fa-physicallocation=9061699&fa-geointent=&gad_source=1",
-          }
-        }
+          },
       }
     },
     {
       "day": "13 Kiss Day",
       "event": "Seal your love with a sweet kiss",
       "additionalInfo": {
-        "Kiss day": {
+        "Best locations For the day": {
+          "Perfect place for the day": {
+            "JHEEL PARK": "https://www.google.com/maps/dir/Netaji+Subhas+University+of+Technology,+Azad+Hind+Fauj+Marg,+Dwarka+Sector-3,+Dwarka,+Delhi/269,+Manak+Vihar,+Tihar+Village,+New+Delhi,+Delhi,+110018/@28.6202743,77.0508286,14z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390d05dd375e5a13:0x108adaa3abe4bd07!2m2!1d77.0379647!2d28.6100216!1m5!1m1!1s0x390d035eb91789c7:0x5adc9b62d1e8a3c6!2m2!1d77.107031!2d28.6309688?entry=ttu",
+            "ROSE CAFE": "https://www.google.com/maps/dir/Netaji+Subhas+University+of+Technology,+Azad+Hind+Fauj+Marg,+Dwarka+Sector-3,+Dwarka,+Delhi/Rose+Cafe,+Westend+Marg,+Butterfly+Park,+Saiyad+Ul+Ajaib+Extension,+Saket,+New+Delhi,+Delhi/@28.5639961,77.0369939,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390d05dd375e5a13:0x108adaa3abe4bd07!2m2!1d77.0379647!2d28.6100216!1m5!1m1!1s0x390ce1e407a7bc3f:0xe4daa7dba1c7f579!2m2!1d77.1982881!2d28.517792?entry=ttu",
+            "VEGAS MALL": "https://www.google.com/maps/dir/Netaji+Subhas+University+of+Technology,+Azad+Hind+Fauj+Marg,+Dwarka+Sector-3,+Dwarka,+Delhi/Vegas+Mall,+Pocket+1,+Sector+14+Dwarka,+Dwarka,+Delhi/@28.6046357,77.0285702,16z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390d05dd375e5a13:0x108adaa3abe4bd07!2m2!1d77.0379647!2d28.6100216!1m5!1m1!1s0x390d1dd61eb460af:0xe89377d97456bc5e!2m2!1d77.029572!2d28.6007474?entry=ttu",
+          },
+        },
+        "Get you corner tickets fast": {
           "BOOK MOVIE": {
             "BOOK MY SHOW":
                 "https://in.bookmyshow.com/explore/home/national-capital-region-ncr",
             "PVR": "https://www.pvrcinemas.com/",
             "JUST DIAL": "https://www.justdial.com/Delhi/PVR-INOX/"
           },
-          "perfect places": [
-            "JHEEL PARK",
-            "ROSE CAFE",
-            "VEGAS MALL",
-          ],
         }
       }
     },
